f385935f3d6f255f4ccfb6e194fd28ad90a67d55
meanseo <chlalstjok97@gmail.com>
2022-06-09 17:43:14 +0900

    15:46

--- users/models.py
+++ users/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 
+from django.db import models
+
 class User(models.Model):
     use_in_migrations = True
-    username = models.CharField
-    password = models.CharField
-    name = models.TextField
-    email = models.TextField
-    regDate = models.TextField
+    username = models.CharField(primary_key=True, max_length=10)
+    password = models.CharField(max_length=10)
+    name = models.TextField()
+    email = models.TextField()
+    regDate = models.DateField()
 
     class Meta:
         db_table="users"
