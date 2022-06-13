a217d43b5db35d66ca20f0702d2d5c90c7245c52
meanseo <chlalstjok97@gmail.com>
2022-06-13 18:27:42 +0900

    18:26 crime save_cctv_norm1

--- crime/crime.py
+++ crime/crime.py
@@ -1,5 +1,6 @@
 import pandas as pd
-
+import numpy as np
+from sklearn import preprocessing
 from context.domains import Reader, File
 import folium
 
@@ -10,7 +11,6 @@ class Solution(Reader):
         # self.reader = Reader()
         self.crime_rate_columns = ['살인검거율', '강도검거율', '강간검거율', '절도검거율', '폭력검거율']
         self.crime_columns = ['살인', '강도', '강간', '절도', '폭력']
-        # self.file_context = './data/'
         self.file.context = './data/'
 
     def hook(self):
@@ -31,6 +31,8 @@ class Solution(Reader):
                 self.folium_test()
             if menu == '3':
                 self.save_cctv_pos()
+            if menu == '4':
+                self.save_cctv_norm()
             elif menu == '0':
                 break
 
@@ -106,7 +108,7 @@ class Solution(Reader):
         '''
         혜화서
         '''
-        crime.to_csv('./save/police_pos.csv')
+        crime.to_csv('./save/police_pos.csv', index=False) # 인덱스 랜덤 생성 방지
 
     def save_cctv_pos(self):
         file = self.file
@@ -138,19 +140,94 @@ class Solution(Reader):
         cctv.drop(cctv.columns[2:6], axis=1, inplace=True)
         # cctv.drop(['2013년도 이전','2014년','2015년','2016년'], axis=1, inplace=True)
         # pop.dropna() NaN 값이 하나라도 있으면 날리는 메소드 / pop.dropna(how ='all') 전부 NaN이면 날리는 메소드
-        print(cctv)
-        print(pop)
-
-        pop['외국인비율'] = pop['외국인'] / pop['인구수'] * 100
         pop['고령자비율'] = pop['고령자'] / pop['인구수'] * 100
+        pop['외국인비율'] = pop['외국인'] / pop['인구수'] * 100
         cctv_pop = pd.merge(cctv, pop, on='구별')
         print(cctv_pop)
+        cor1 = cctv_pop['고령자비율'].corr(cctv_pop['소계'])
+        cor2 = cctv_pop['외국인비율'].corr(cctv_pop['소계'])
 
-        cctv_pop = pd.DataFrame()
+        print(f'고령자비율과 CCTV의 상관계수 {str(cor1)} \n'
+              f'외국인비율과 CCTV의 상관계수 {str(cor2)} ')
+        """
+        cor1 = np.corrcoef(cctv_pop['고령자비율'], cctv_pop['소계'])
+        cor2 = np.corrcoef(cctv_pop['외국인비율'], cctv_pop['소계'])
+        print(f'고령자비율과 CCTV의 상관계수 {str(cor1)} \n'
+              f'외국인비율과 CCTV의 상관계수 {str(cor2)} ')
+        
+         고령자비율과 CCTV 의 상관계수 [[ 1.         -0.28078554]
+                                     [-0.28078554  1.        ]] 
+         외국인비율과 CCTV 의 상관계수 [[ 1.         -0.13607433]
+                                     [-0.13607433  1.        ]]
+        r이 -1.0과 -0.7 사이이면, 강한 음적 선형관계,
+        r이 -0.7과 -0.3 사이이면, 뚜렷한 음적 선형관계,
+        r이 -0.3과 -0.1 사이이면, 약한 음적 선형관계,
+        r이 -0.1과 +0.1 사이이면, 거의 무시될 수 있는 선형관계,
+        r이 +0.1과 +0.3 사이이면, 약한 양적 선형관계,
+        r이 +0.3과 +0.7 사이이면, 뚜렷한 양적 선형관계,
+        r이 +0.7과 +1.0 사이이면, 강한 양적 선형관계
+        고령자비율 과 CCTV 상관계수 [[ 1.         -0.28078554] 약한 음적 선형관계
+                                    [-0.28078554  1.        ]]
+        외국인비율 과 CCTV 상관계수 [[ 1.         -0.13607433] 거의 무시될 수 있는
+                                    [-0.13607433  1.        ]]                        
+         """
+
         cctv_pop.to_csv('./save/cctv_pop.csv')
 
     def save_cctv_norm(self):
-        pass
+        '''
+        표준화는 데이터의 평균을 0, 표준편차를 1로 만드는 것이다.
+        x = (x - mu) / sigma
+        scale = (x - np.mean(x, axis=0)) / np.std(x, axis=0)
+        정규화는 최대값을 1, 최솟값을 0으로 만드는 것이다.
+        '''
+        file = self.file
+        file.context = './save/'
+        file.fname = 'police_pos'
+        police_pos = self.csv(file)
+        police = pd.pivot_table(police_pos, index='구별', aggfunc=np.sum)
+        police['살인검거율'] = police['살인 검거'] / police['살인 발생'] * 100
+        police['강도검거율'] = police['강도 검거'] / police['강도 발생'] * 100
+        police['강간검거율'] = police['강간 검거'] / police['강간 발생'] * 100
+        police['절도검거율'] = police['절도 검거'] / police['절도 발생'] * 100
+        police['폭력검거율'] = police['폭력 검거'] / police['폭력 발생'] * 100
+        police.drop(['살인 검거', '강도 검거', '강간 검거', '절도 검거', '폭력 검거'], axis=1, inplace=True)
+        print(police)
+        for j, i in enumerate(self.crime_rate_columns):
+            police[i].loc[j, i] = 100
+        # police.loc[police[self.crime_rate_columns] > 100] = 100
+        print(police)
+        police.rename(columns={
+            '살인 발생': '살인',
+            '강도 발생': '강도',
+            '강간 발생': '강간',
+            '절도 발생': '절도',
+            '폭력 발생': '폭력'
+        }, inplace=True)
+        x = police[self.crime_rate_columns]
+        min_max_scalar = preprocessing.MinMaxScaler()
+        """     
+        피쳐 스케일링(Feature scalining)은 해당 피쳐들의 값을 일정한 수준으로 맞춰주는 것이다.
+        이때 적용되는 스케일링 방법이 표준화(standardization) 와 정규화(normalization)다.
+        
+        1단계: 표준화(공통 척도)를 진행한다.
+            표준화는 정규분포를 데이터의 평균을 0, 분산이 1인 표준정규분포로 만드는 것이다.
+            x = (x - mu) / sigma
+            scale = (x - np.mean(x, axis=0)) / np.std(x, axis=0)
+        2단계: 이상치 발견 및 제거
+        3단계: 정규화(공통 간격)를 진행한다.
+            정규화에는 평균 정규화, 최소-최대 정규화, 분위수 정규화가 있다.
+             * 최소최대 정규화는 모든 데이터를 최대값을 1, 최솟값을 0으로 만드는 것이다.
+            도메인은 데이터의 범위이다.
+            스케일은 데이터의 분포이다.
+            목적은 도메인을 일치시키거나 스케일을 유사하게 만든다.     
+        """
+        x_scaled = min_max_scalar.fit_transform(x.astype(float))
+        police_norm = pd.DataFrame(x_scaled, columns=self.crime_columns, index=police.index)
+        police_norm[self.crime_rate_columns] = police[self.crime_rate_columns]
+        police_norm['범죄'] = np.sum(police_norm[self.crime_rate_columns], axis=1)
+        police_norm['검거'] = np.sum(police_norm[self.crime_columns], axis=1)
+        # police_norm.to_csv('./save/police_norm.csv', sep=',', encoding='UTF-8')
 
     def folium_test(self):
         file = self.file
