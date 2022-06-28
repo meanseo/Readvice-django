e996bc6fc07ce88e6fdc311abb076b15c678144c
meanseo <chlalstjok97@gmail.com>
2022-06-28 16:01:07 +0900

    2022-06-28 subclass 기법

--- tf2/hand_writing.py
+++ tf2/hand_writing.py
@@ -1,26 +1,28 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from keras import datasets
 from tensorflow import keras
-from tensorflow.keras import datasets, layers, models
+from keras import datasets, layers, models
+
 
 class Solution:
     def __init__(self):
         self.mnist = keras.datasets.mnist
-
-        # preprocessing 앞으로 init에 넣을 것 임
         (train_images, train_labels), (test_images, test_labels) = datasets.mnist.load_data()
 
         train_images = train_images.reshape((60000, 28, 28, 1))
         test_images = test_images.reshape((10000, 28, 28, 1))
 
         # 픽셀 값을 0~1 사이로 정규화합니다.
-        train_images, test_images = train_images / 255.0, test_images / 255.0
+        self.train_images, self.test_images = train_images / 255.0, test_images / 255.0
+        self.layers = layers
+        self.model = models.Sequential()
+        self.train_labels = train_labels
 
     def solution(self):
-        models = keras.models
-        layers = keras.layers
-        model = models.Sequential()
+        self.modeling()
+        self.training()
+
+    def modeling(self):
+        model = self.model
+        layers = self.layers
         model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
         model.add(layers.MaxPooling2D((2, 2)))
         model.add(layers.Conv2D(64, (3, 3), activation='relu'))
@@ -31,3 +33,14 @@ class Solution:
         model.add(layers.Dense(10, activation='softmax'))
 
         model.summary()
+
+    def training(self):
+        model = self.model
+        model.compile(optimizer='adam',
+                      loss='sparse_categorical_crossentropy',
+                      metrics=['accuracy'])
+
+        model.fit(self.train_images, self.train_labels, epochs=5)
+
+if __name__ == '__main__':
+    Solution().solution()
